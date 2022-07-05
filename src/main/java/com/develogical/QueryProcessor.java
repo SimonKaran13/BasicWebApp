ab8388586b0df4c11bead1988dcad01a728ac4c7
Simon <simon.karan@tum.de>
2022-07-05 10:23:13 +0200

    added !

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -9,7 +9,7 @@ public class QueryProcessor {
                     "writer in the English language and the world's pre-eminent dramatist.";
         }
         if (query.toLowerCase().contains("hello")) {
-            return "world";
+            return "world!";
         }
         return "";
     }
